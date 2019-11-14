3cc84b1e67491a52bf5f9a5e566f4ecba11df271
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-14 14:58:11 -0500

    initial commit, route for update form and update form with test div made

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateMovie = () => {
+  return (
+    <div className="update-form">
+      <h1>testing route</h1>
+    </div>
+  );
+};
+
+export default UpdateMovie;
